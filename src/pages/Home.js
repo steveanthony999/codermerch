98530506a2fe945a7089c888fbae6abb7dd85bc2
Steven Woodward <steveanthony999@gmail.com>
2022-02-14 23:43:05 -0800

    designing boxes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,40 @@
+import { Link } from 'react-router-dom';
+
 import './Home.css';
 
 const Home = () => {
   return (
     <div className='Home'>
+      {/* TITLE */}
+      {/* TITLE */}
+      {/* TITLE */}
       <h1 className='Home-title'>coderMerch</h1>
+      {/* BOX */}
+      {/* BOX */}
+      {/* BOX */}
+      <div className='box'>
+        <span></span>
+        <div className='content'>
+          <h2>Card One</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <Link to='/'>Details</Link>
+        </div>
+      </div>
+
+      <div className='box'>
+        <span></span>
+        <div className='content'>
+          <h2>Card One</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <Link to='/'>Details</Link>
+        </div>
+      </div>
     </div>
   );
 };
