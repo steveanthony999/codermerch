7dbecac4ff47af07241b1325f77b043832ba9e61
Steven Woodward <steveanthony999@gmail.com>
2022-02-16 14:31:22 -0800

    made responsive

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,44 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
 
 import './Home.css';
 
+const url =
+  'https://codermerch.us14.list-manage.com/subscribe/post?u=664a41c1d6f78be699ec13cd0&amp;id=8e0d088b73';
+
+const SimpleForm = () => <MailchimpSubscribe url={url} />;
+
 const Home = () => {
+  useEffect(() => {
+    document.querySelector('input[type="email"]').placeholder =
+      'Enter your email';
+  }, []);
+
   return (
     <div className='Home'>
-      {/* TITLE */}
-      {/* TITLE */}
-      {/* TITLE */}
       <h1 className='Home-title'>coderMerch</h1>
-      {/* BOX */}
-      {/* BOX */}
-      {/* BOX */}
-      <div className='box'>
-        <span></span>
-        <div className='content'>
-          <h2>Card One</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-          <Link to='/'>Details</Link>
-        </div>
-      </div>
-
-      <div className='box'>
-        <span></span>
-        <div className='content'>
-          <h2>Card One</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
-          <Link to='/'>Details</Link>
-        </div>
+      <h2 className='Home-subtitle'>First drop - 03.02.2022</h2>
+      <div className='Home-container'>
+        <MailchimpSubscribe
+          url={url}
+          render={({ subscribe, status, message }) => (
+            <div className='signup-form'>
+              <SimpleForm onSubmitted={(formData) => subscribe(formData)} />
+              {status === 'sending' && (
+                <div style={{ color: 'white' }}>sending...</div>
+              )}
+              {status === 'error' && (
+                <div
+                  style={{ color: 'white' }}
+                  dangerouslySetInnerHTML={{ __html: message }}
+                />
+              )}
+              {status === 'success' && (
+                <div style={{ color: 'white' }}>Subscribed !</div>
+              )}
+            </div>
+          )}
+        />
       </div>
     </div>
   );
