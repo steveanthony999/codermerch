3435a34f02d68ef4a42f236290f8207a5e633a9e
Steven Woodward <steveanthony999@gmail.com>
2022-02-24 11:30:08 -0800

    added navbar skeleton

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav className='Navbar'>
+      <ul>
+        <li>Nav Item 1</li>
+        <li>Nav Item 2</li>
+        <li>Nav Item 3</li>
+        <li>Nav Item 4</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
