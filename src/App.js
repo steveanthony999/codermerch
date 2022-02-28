1839d88aab0724c091ee1d00cdbaa617f3c06a95
Steven Woodward <steveanthony999@gmail.com>
2022-02-27 20:58:29 -0800

    reset to in development

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import CheckoutPage from './pages/CheckoutPage';
 import './App.css';
 
 function App() {
-  const [inDevelopment, setInDevelopment] = useState(false);
+  const [inDevelopment, setInDevelopment] = useState(true);
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
   const [order, setOrder] = useState({});
